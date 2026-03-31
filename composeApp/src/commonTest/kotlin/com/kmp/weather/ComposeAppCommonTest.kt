11cf93087d8be6bc6e6633da9d0a1501cf1626bf
PatelDipen <dipen.d.patel@accenture.com>
2026-03-31 14:34:49 +0300

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/kmp/weather/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.kmp.weather
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
