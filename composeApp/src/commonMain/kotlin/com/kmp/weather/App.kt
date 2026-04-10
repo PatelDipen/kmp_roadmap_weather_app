17b060dca722f9ca50af1d35e201dd766b6e357f
dipen.d.patel <dipen.d.patel@accenture.com>
2026-04-10 12:24:45 +0300

    Implemented splash screen

--- composeApp/src/commonMain/kotlin/com/kmp/weather/App.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/weather/App.kt
@@ -1,29 +1,32 @@
 package com.kmp.weather
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import com.kmp.weather.presentation.main.MainScreen
+import com.kmp.weather.presentation.splash.SplashScreen
+import kotlinx.coroutines.delay
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        var isStartupComplete by remember { mutableStateOf(false) }
 
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
+        LaunchedEffect(Unit) {
+            delay(2000)
+            isStartupComplete = true
+        }
 
-
+        if (isStartupComplete) {
+            MainScreen()
+        } else {
+            SplashScreen()
         }
     }
 }
